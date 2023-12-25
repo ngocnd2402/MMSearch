f7c9091e717ed923b7d7acb3f83ef88f1dbbc146
blkhanhlinh <linhbui8103@gmail.com>
2023-12-26 00:31:42 +0700

    update UI

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,11 +1,17 @@
 "use client"
 
 import React from 'react';
+import Gallery from "@/components/Gallery";
+import Sidebar from "@/components/Sidebar";
+import { ResultDataProvider } from '@/context/provider';
 
 export default function Home() {
   return (
-    <div>
-        MMSearch
-    </div>
+    <ResultDataProvider>
+      <div className='flex bg-blue-100'>
+        <Sidebar />
+        <Gallery />     
+      </div> 
+    </ResultDataProvider>
   );
 }
