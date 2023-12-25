275362554e755c2fc23898e366fe95065fad677b
blkhanhlinh <linhbui8103@gmail.com>
2023-12-25 23:53:43 +0700

    init frontend

---
+++ frontend/app/layout.jsx
@@ -0,0 +1,19 @@
+import { Open_Sans } from 'next/font/google'
+import "../styles/index.css"
+
+const openSans = Open_Sans({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'MMSearch',
+  description: 'Video Browser website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={openSans.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
